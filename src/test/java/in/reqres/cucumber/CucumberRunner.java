6c963cbc747a935a84df868fd6877cf8b0a7e6f9
bella-shah <shahbella455@gmail.com>
2022-06-21 12:48:53 +0100

    Reqres-Serenity-New-CucumberWeek19

---
+++ src/test/java/in/reqres/cucumber/CucumberRunner.java
@@ -0,0 +1,14 @@
+package in.reqres.cucumber;
+
+import cucumber.api.CucumberOptions;
+import in.reqres.testbase.TestBase;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/java/resources/feature",//path from feature file
+ tags = "@SMOKE")
+public class CucumberRunner extends TestBase {
+
+}
